3c5c31c84085665cd4fe5ecefba150e7c68fdfd4
Jane Suh <sourcherryyogurt@gmail.com>
2019-11-08 19:50:00 -0500

    Displaying data from characters endpoint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from "./components/Header.js";
-
+import WelcomePage from './components/WelcomePage';
+import CharacterList from './components/CharacterList';
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path='/' component={WelcomePage} />
+      {/* <Route path='/characters' component={CharacterList} /> */}
+      <CharacterList />
     </main>
-  );
+  )
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+export default function CharacterCard({ character }) {
+  // return <span>todo: character</span>;
+  return (
+    <div className='character-card'>
+      <h2>Known As: {character.name}</h2>
+      <img src={character.image} alt={character.name} />
+      <p>Status: {character.status}</p>
+      <p>From: {character.origin.name}</p>
+      <p>Last Seen: {character.location.name}</p>
+    </div>
+  );
+};
